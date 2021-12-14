c5f4d6f46303c4194432326a2f812c13448d7af3
Bartosz Kardas <kardasbart@gmail.com>
2021-12-15 00:15:51 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { Typography, Container } from "@mui/material";
+
 export default function Footer() {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    // <Container display="flex" flexGrow={1} justifyContent="flex-end">
+    <Typography variant="h6" component="div" textAlign="center" m={5}>
+      Copyright © 2021 Bartosz&nbsp;Kardas All&nbsp;Rights&nbsp;Reserved
+    </Typography>
+    // </Container>
   );
 }
