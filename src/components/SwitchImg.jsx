df6ee92fa6a6c49a92677dbdd546f248c514c7f9
Bartosz Kardas <kardasbart@gmail.com>
2022-01-17 19:46:58 +0100

    Disabled switchimg

--- src/components/SwitchImg.jsx
+++ src/components/SwitchImg.jsx
@@ -6,34 +6,12 @@ export default function SwitchImg(props) {
   console.log(props.primary);
   console.log(props.secondary);
   return (
-    <Carousel
-      duration={200}
-      interval={5000}
-      swipe={false}
-      indicatorContainerProps={{
-        style: {
-          bottom: "1rem",
-          marginTop: "0px",
-          position: "absolute",
-          textAlign: "center", // 4
-          zIndex: 99,
-        },
+    <GatsbyImage
+      image={props.primary}
+      alt="Primary image"
+      style={{
+        height: "300px",
       }}
-    >
-      <GatsbyImage
-        image={props.primary}
-        alt="Primary image"
-        style={{
-          height: "300px",
-        }}
-      />
-      <GatsbyImage
-        image={props.secondary}
-        alt="Primary image"
-        style={{
-          height: "300px",
-        }}
-      />
-    </Carousel>
+    />
   );
 }
